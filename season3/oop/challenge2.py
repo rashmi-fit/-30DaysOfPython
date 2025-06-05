2c6a758d06f349efdc212aa787ec64dcc8495c0f
rashmi-fit <rashmim282@gmail.com>
2025-06-05 21:56:56 +0100

    Day 9 Polymorphism

--- season3/oop/challenge2.py
+++ season3/oop/challenge2.py
@@ -1,22 +1,31 @@
 # Extend Car into an ElectricCar subclass with battery capacity
-# class vehicle:
-#       def general_usage(self):
-#             print('general use of vehicle: transporatation')
 
 class car:
       def general_features(self):
             print('general features of car: 4 wheels, steering wheel, seats')
-
+# below function i have used so that same can be used in child so that it can be overridden
+      def specific_usage(self):
+            self.general_features()
+            print("Specific use of car: General transportation")
 class electric_car(car):
       def __init__(self, battery_capacity):
             self.battery_capacity = battery_capacity
             print(f'I am an electric car with a battery capacity of {self.battery_capacity} kWh')
-
+# here method over riding happening
       def specific_usage(self):
             self.general_features()
             print('specific use of electric car: driving with electric power')
 
-# my_electric_car = electric_car(75)
 
+def describe_car_usage(car_obj):
+    car_obj.specific_usage()
+
+
+car1 = car()
 e = electric_car(75)
-e.specific_usage
+
+print("\nDescribing car1:")
+describe_car_usage(car1)
+
+print("\nDescribing electric1:")
+describe_car_usage(e)
